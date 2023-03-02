86ffaac2ff0cab9a0cd834b83cf1f44751590aca
Rifqi Harits <rharits18@gmail.com>
2023-03-02 18:39:14 +0700

    first commit

---
+++ Tugas 2/a.php/index.php
@@ -0,0 +1,28 @@
+<?php
+$namadepan = "Rifqi";
+$namabelakang = "Harits";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas2a</title>
+</head>
+<body>
+    <?php 
+    for ($i = 1; $i <= 100; $i++) {
+        if ($i % 3 == 0 && $i % 5 == 0) {
+            echo $namadepan . " " . $namabelakang . "<br>";
+        } elseif ($i % 3 == 0) {
+            echo $namadepan . "<br>";
+        } elseif ($i % 5 == 0) {
+            echo $namabelakang . "<br>";
+        } else {
+            echo $i . "<br>";
+        }
+    }
+    ?>
+</body>
+</html>
---
+++ pertemuan2/index.php
@@ -0,0 +1,22 @@
+<?php 
+ $nama = "Harits";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<h1>
+    <?php echo "Hello $nama"; ?>
+</h1>
+
+    <p><?php echo "pemrograman web"; ?></p>
+    <P><?PHP echo "10"; ?></p>
+
+</body>
+</html>
